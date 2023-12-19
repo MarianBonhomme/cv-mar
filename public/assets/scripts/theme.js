a26f82655f7cfe95c6f23965c138b0cb5f452eb6
Marian Bonhomme <marian.bonhomme@gmail.com>
2023-12-19 15:50:57 +0100

    theme

--- public/assets/scripts/theme.js
+++ public/assets/scripts/theme.js
@@ -4,51 +4,30 @@ document.addEventListener('DOMContentLoaded', function() {
 	const settingsButton = document.getElementById('settings-button');
 	const settingsDropdown = document.getElementById('settings-dropdown');
 
-	const waveTheme = document.getElementById('wave-theme');
-	const liquidTheme = document.getElementById('liquid-theme');
-
 	const colorButtons = document.querySelectorAll('.color-button');
-	const themeSwitcherButtons = document.querySelectorAll('#theme-switcher-button')
 
-	var currentTheme = 'liquid';
-	var currentColor = 'green';
-	var currentHexa = '#209073';
+	var currentTheme = 'wave';
+	var currentColor = 'purple';
+	var currentHexa = '#664EAC';
 
 	switchColor(currentTheme, currentColor, currentHexa);
 
-	// open dropdown
 	settingsButton.addEventListener('click', () => {
 		settingsDropdown.classList.toggle('hidden');
 	});
 
-	// switch color
 	colorButtons.forEach(item => {
 		item.addEventListener('click', () => {
-			const color = item.getAttribute('data-color');
-			const hexa = item.getAttribute('data-hexa');
-			currentColor = color;
-			currentHexa = hexa;
-			switchColor(currentTheme, color, hexa);
+			const theme = item.getAttribute('theme');
+			const color = item.getAttribute('color');
+			const hexa = item.getAttribute('hexa');
+			switchColor(theme, color, hexa);
 		});
 	});
-
-	//switch theme
-	themeSwitcherButtons.forEach(button => {
-		button.addEventListener('click', () => {
-			switchTheme();
-		})
-	});	
-
 
 	function switchColor(theme, color, hexa) {
 		root.style.setProperty('--background-url', `url('../imgs/bgs/${theme}-${color}.jpg')`);
 		root.style.setProperty('--color-primary', hexa);
-	};	
-
-	function switchTheme() {
-		(currentTheme === 'liquid') ? currentTheme = 'wave' : currentTheme = 'liquid';
-		switchColor(currentTheme, currentColor, currentHexa);
-		waveTheme.classList.toggle('hidden');
-		liquidTheme.classList.toggle('hidden');
-	}
+	};
+	
 });
