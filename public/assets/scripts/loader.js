1f0db9e0bab1ba85f04f5c3d20e7cb8671cf499a
Marian Bonhomme <marian.bonhomme@gmail.com>
2023-12-19 16:25:55 +0100

    fix chemin

--- public/assets/scripts/loader.js
+++ public/assets/scripts/loader.js
@@ -2,22 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	function preloadImages() {
 		const images = [
-			'../imgs/bgs/wave-purple.jpg',
-			'../imgs/bgs/wave-red.jpg',
-			'../imgs/bgs/wave-green.jpg',
-			'../imgs/bgs/wave-blue.jpg',
-			'../imgs/bgs/liquid-purple.jpg',
-			'../imgs/bgs/liquid-red.jpg',
-			'../imgs/bgs/liquid-green.jpg',
-			'../imgs/bgs/liquid-blue.jpg',
+			'../asets/imgs/bgs/wave-purple.jpg',
+			'../asets/imgs/bgs/wave-red.jpg',
+			'../asets/imgs/bgs/wave-green.jpg',
+			'../asets/imgs/bgs/wave-blue.jpg',
+			'../asets/imgs/bgs/liquid-purple.jpg',
+			'../asets/imgs/bgs/liquid-red.jpg',
+			'../asets/imgs/bgs/liquid-green.jpg',
+			'../asets/imgs/bgs/liquid-blue.jpg',
 		];
 		images.forEach((image) => {
 			new Image().src = image;
-			console.log('load');
 		});
 		console.log('all loaded')
-	}
+	};
 
 	preloadImages();
-
 });
